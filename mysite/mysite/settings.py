83d04d931531ec28b1e72e802c73820db630527a
unknown <kcauqednairb@gmail.com>
2022-06-29 15:26:49 +0200

    logout update2

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -145,6 +145,7 @@ if DEBUG == False:
 MEDIA_URL='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+LOGOUT_REDIRECT_URL = '/login112'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
